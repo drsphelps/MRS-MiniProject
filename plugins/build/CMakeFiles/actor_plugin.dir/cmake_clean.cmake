1c284eb7c095b76270740ea3327590266395c1bf
andreea794 <aeb83@cam.ac.uk>
2020-03-04 10:05:42 +0000

    Added human actor following predefined trajectory.

---
+++ plugins/build/CMakeFiles/actor_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/actor_plugin.dir/actor_plugin.cc.o"
+  "libactor_plugin.pdb"
+  "libactor_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/actor_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
